2d8dacb50b24d4e9d52591acb5b26a826cf87d70
mahbubsakib <sakibmahbub92@gmail.com>
2024-12-24 22:03:21 +0600

    attempted assignments

--- src/pages/MyAttemptedAssignments/MyAttemptedAssignments.jsx
+++ src/pages/MyAttemptedAssignments/MyAttemptedAssignments.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const MyAttemptedAssignments = () => {
+
+    const { user } = useContext(AuthContext);
+    const [submissions, setSubmissions] = useState();
+    const [startDate, setStartDate] = useState(new Date());
+
+    const fetchSubmittedData = async () => {
+        const { data } = await axios.get(`${import.meta.env.VITE_SERVER_URL}/submitted-assignment/${user.email}`);
+        setSubmissions(data);
+        setStartDate(new Date(data.due));
+    }
+
+    useEffect(() => {
+        fetchSubmittedData();
+    }, []);
+    console.log(submissions);
+
     return (
-        <div>
-            My Attempted Assignments
+        <div className="w-10/12 mx-auto my-5">
+        <h2 className="text-xl font-semibold text-center mb-5">My Attempted Assignments</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Title</th>
+                            <th>Status</th>
+                            <th>Total Marks</th>
+                            <th>Obtained Marks</th>
+                            <th>Feedback</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            submissions && (
+                            submissions.map((submission, index) => <tr key={submission._id}>
+                            <th>{index+1}</th>
+                            <td>{submission.title}</td>
+                            <td>{submission.status}</td>
+                            <td>{submission.marks}</td>
+                            <td>{submission.obtainedMarks ? submission.obtainedMarks : 'Not Graded Yet'}</td>
+                            <td>{submission.feedback ? submission.feedback : '-'}</td>
+                        </tr>)
+                        )}
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
